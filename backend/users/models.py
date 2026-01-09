05c4512894857b4401b19c7dfb00bdd99b5842af
Khalimovgeek <j94341918@gmail.com>
2026-01-09 12:05:09 +0530

    Configure PostgreSQL database

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 # Create your models here.
+
+
+class Users(AbstractBaseUser,BaseUserManager):
+
+    username = models.CharField(null=False, max_length=240)
+    
