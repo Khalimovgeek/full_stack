907f7584e2aad4d7095065f5303705bea609eb86
Khalimovgeek <j94341918@gmail.com>
2026-01-09 16:48:49 +0530

    crud api for delete update

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TaskListCreateAPI
+from .views import TaskListCreateAPI,TaskDeleteOrUpdateAPI
 
 
 urlpatterns =[
     path("api/v1/tasks/", TaskListCreateAPI.as_view()),
+    path("api/v1/tasks/<int:pk>/", TaskDeleteOrUpdateAPI.as_view()),
+
 ]
 
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+from django.shortcuts import get_object_or_404
+
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -29,5 +31,31 @@ class TaskListCreateAPI(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class TaskDeleteOrUpdateAPI(APIView):
+    permission_classes = [IsAuthenticated, IsOwnerOrAmdin]
+
+    def get_object(self, pk):
+        return get_object_or_404(Task, pk=pk)
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        self.check_object_permissions(request, task)
+        return Response(TaskSerializer(task).data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        self.check_object_permissions(request, task)
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        self.check_object_permissions(request, task)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
